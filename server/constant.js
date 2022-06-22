0de0c54011e30fb8a62d57e321e97e7bd302f7db
Gaurav Chauriya <chauriyagaurav@gmail.com>
2022-06-22 18:38:45 +0530

    Add files via upload

--- server/constant.js
+++ server/constant.js
@@ -6,5 +6,19 @@ module.exports = {
     },
     KEYS: {
         OWM_APP_KEY: "5221e841812ff9156297de4378babbdf"
+    },
+    Month: {
+        january: "01",
+        february: "02",
+        march: "03",
+        april: "04",
+        may: "05",
+        june: "06",
+        july: "07",
+        august: "08",
+        september: "09",
+        october: "10",
+        november: "11",
+        december: "12",
     }
 }

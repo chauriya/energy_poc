05592ccee0ace0368c20ba41124db0f9ef556aa9
Gaurav Chauriya <chauriyagaurav@gmail.com>
2022-06-22 18:37:58 +0530

    Add files via upload

--- server/handler/index.js
+++ server/handler/index.js
@@ -15,28 +15,45 @@ module.exports = {
         try {
             if(req.query.location) {
                 const location = await axios.get(`${constants.SERVER_URL.OPEN_WEATHER_MAP}/geo/1.0/direct?q=${req.query.location}&limit=1&appid=${constants.KEYS.OWM_APP_KEY}`);
-                const lat = parseFloat(location.data[0].lat);
-                const lon = parseFloat(location.data[0].lon);
-                const d = new Date();
-                const year = req.query.year || d.getFullYear();
-                const { data } = await axios.get(`${constants.SERVER_URL.NASA_POWER}/api/temporal/daily/regional?latitude-min=${lat - 1}&latitude-max=${lat + 1}&longitude-min=${lon - 1}&longitude-max=${lon + 1}&parameters=T2M&community=SB&start=${year}0101&end=${year}1231&format=JSON`);
-                let approxLat = 0;
-                let approxLon = 0;
-                if(lat%1 > 0.50) {
-                    approxLat = parseInt(lat) + 0.75;
+                if(location.data.length) {
+                    const lat = parseFloat(location.data[0].lat);
+                    const lon = parseFloat(location.data[0].lon);
+                    const d = new Date();
+                    const year = req.query.year || d.getFullYear();
+                    const month = constants.Month[req.query.month] || d.getMonth() + 1;
+                    d.setFullYear(year, month, 0);
+                    const lastdate = d.getDate()
+                    const { data } = await axios.get(`${constants.SERVER_URL.NASA_POWER}/api/temporal/daily/regional?latitude-min=${lat - 1}&latitude-max=${lat + 1}&longitude-min=${lon - 1}&longitude-max=${lon + 1}&parameters=T2M&community=SB&start=${year}${month}01&end=${year}${month}${lastdate}&format=JSON`);
+                    let approxLat = 0;
+                    let approxLon = 0;
+                    if(lat%1 > 0.50) {
+                        approxLat = parseInt(lat) + 0.75;
+                    } else {
+                        approxLat = parseInt(lat) + 0.25;
+                    }
+                    if(lon%1 > 0.50) {
+                        approxLon = parseInt(lon) + 0.75;
+                    } else {
+                        approxLon = parseInt(lon) + 0.25;
+                    }
+                    const result = data.features.filter(function (list){
+                        return list.geometry.coordinates.filter(value => [approxLat, approxLon].includes(value)).length === 2;
+                    })
+                    res.json({
+                        ...result[0],
+                        lat,
+                        approxLat,
+                        lon,
+                        approxLon,
+                        name: location.data[0].name,
+                        country: location.data[0].country,
+                        state: location.data[0].state,
+                        year,
+                        month
+                    });
                 } else {
-                    approxLat = parseInt(lat) + 0.25;
+                    res.status(400).json("Location not found!!");
                 }
-                if(lon%1 > 0.50) {
-                    approxLon = parseInt(lon) + 0.75;
-                } else {
-                    approxLon = parseInt(lon) + 0.25;
-                }
-                const result = data.features.filter(function (list){
-                    return list.geometry.coordinates.filter(value => [approxLat, approxLon].includes(value)).length === 2;
-                })
-                res.json({...result[0], lat,approxLat, lon, approxLon});
-                
             } else {
                 res.status(400).json("Query parameter 'location' is required");
             }
